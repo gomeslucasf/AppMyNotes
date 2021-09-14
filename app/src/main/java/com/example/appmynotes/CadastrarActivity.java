ac54158ac7ffbc455461e5c40bab400d90c268b7
Lucas Gomes <gomesfeliplucas@gmail.com>
2021-09-13 21:01:48 -0300

    Implementação da tela cadastrar

--- app/src/main/java/com/example/appmynotes/CadastrarActivity.java
+++ app/src/main/java/com/example/appmynotes/CadastrarActivity.java
@@ -2,7 +2,9 @@ package com.example.appmynotes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class CadastrarActivity extends AppCompatActivity {
 
@@ -10,5 +12,21 @@ public class CadastrarActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastrar);
+    }
+    @Override
+    public boolean onOptionsItemSelected( MenuItem item) {
+        switch (item.getItemId())
+        {
+            case R.id.mi_fechar:
+                finish();
+                break;
+            case R.id.mi_novaAnotacao:
+                Intent intent =new Intent(this,MainActivity.class);
+                startActivity(intent);
+                break;
+
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
